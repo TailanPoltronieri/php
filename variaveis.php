eccc0774a64f0d22187cb154e5c554501cdb0490
Tailan <poltronieritailan@gmail.com>
2024-08-04 13:20:25 -0300

    correcao de quebra de linha

--- variaveis.php
+++ variaveis.php
@@ -4,10 +4,10 @@ $n1 = 2;
 $n2 = 4;
 $r = $n2 - $n1;
 
-echo"O valor de n1 é: ";
+echo"\nO valor de n1 é: ";
 echo $n1;
-echo"O valor de n2 é: ";
+echo"\nO valor de n2 é: ";
 echo $n2;
-echo"O valor de n1 - n2 é: ";
+echo"\nO valor de n2 - n1 é: ";
 echo $r;
 ?>
